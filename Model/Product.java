d72a2d7107bbe516789a6155a1e9b9fc9e3b71e5
EngelAZ <engel.zeledon26@gmail.com>
2026-02-03 16:00:41 -0600

    The attributes of the product object were created, and the procedures that the object would later have were commented out in the object's code.

--- Model/Product.java
+++ Model/Product.java
@@ -1,4 +1,18 @@
 public class Product
 {
+    public int Id;
+    public String Name;
+    public Double Quantity;
+    public float Price;
+    public String Description;
+    public String Category;
+    public boolean Status;
 
+    /*public Editing
+    {
+
+    }
+    public  verification
+
+    */
 }
