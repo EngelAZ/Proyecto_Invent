b20a0ab0d2f9c8cd9ca8786b63848578e2c0594e
EngelAZ <engel.zeledon26@gmail.com>
2026-02-05 15:43:36 -0600

    A constructor for the product object was created and the DataStorage file was created.

--- Model/Product.java
+++ Model/Product.java
@@ -1,12 +1,22 @@
 public class Product
 {
-    public int Id;
-    public String Name;
-    public double Quantity;
-    public int Price;
-    public String Description;
-    public String Category;
-    public boolean Status;
+    private int Id;
+    private  String Name;
+    private double Quantity;
+    private int Price;
+    private String Description;
+    private String Category;
+    private boolean Status;
+
+    public Product(int id, String name, double quantity, int price, String description, String category, boolean status) {
+        this.Id = id;
+        this.Name = name;
+        this.Quantity = quantity;
+        this.Price = price;
+        this.Description = description;
+        this.Category = category;
+        this.Status = status;
+    }
 
     /*public Editing
     {
