44a422a490fcd2b836edb3a5379b210e7e27c5e3
rodriguezJ-eng <alejandrodriguez0710@gmail.com>
2026-02-06 15:17:57 -0600

    Implementación de guardado básico de productos en Data.txt

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
+
 public class Main
 {
     public static void main(String[] args) 
     {
-        
+        // creación del ArrayList
+        ArrayList<Product> productos = new ArrayList<>();
+
+        // prueba para agregar productos (aún no cuento con "Product.java" completo)
+        productos.add(new Product(1, "arroz", 2, 27, "arroz", "alimentos", false));
+
+        // mandamos a llamar DataStorage.save(productos);
+        DataStorage.save(productos);
+
+        System.out.println("Guardado correctamente...");
     }    
 }
---
+++ Model/DataStorage.java
@@ -0,0 +1,24 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class DataStorage 
+{
+    public static void save(ArrayList<Product> productos) 
+    {
+        try (PrintWriter writer = new PrintWriter(new FileWriter("Data.txt"))) 
+        {
+
+            for (int i = 0; i < productos.size(); i++) 
+            {
+                Product p = productos.get(i);
+                writer.println(p.toString());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error al guardar archivo");
+        }
+    }
+}
