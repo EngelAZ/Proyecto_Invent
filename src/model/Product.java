534ad9ac11d60be1ad8fb9894705f24bbaf76b17
EngelAZ <engel.zeledon26@gmail.com>
2026-02-10 21:04:17 -0600

    A default constructor was created in the Product class and the code in that class was commented out.

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,16 +1,63 @@
 package model;
 
+
 public class Product
 {
-//Attributes
+    // Attributes
+    /**
+     * Id del producto
+     */
     private final int id;
+    /**
+     * Nombre del producto
+     */
     private String name;
+    /**
+     * Cantidad del producto
+     */
     private double quantity;
+    /**
+     * Precio del producto
+     */
     private int price;
+    /**
+     * Descripción del producto
+     */
     private String description;
+    /**
+     * Categoría del producto
+     */
     private String category;
+    /**
+     * Estado del producto (true = en stock, false = agotado)
+     */
     private boolean status;
 
+    // Constructors
+    /**
+     * Constructor por defecto
+     */
+    public Product()
+    {
+        this.id = 0;
+        this.name = " ";
+        this.quantity = 0;
+        this.price = 0;
+        this.description = " ";
+        this.category = " ";
+        this.status = false;
+    }
+
+    /**
+     * Constructor con parámetros
+     * @param id - Id del producto
+     * @param name - Nombre del producto
+     * @param quantity - Cantidad del producto
+     * @param price - Precio del producto
+     * @param description - Descripción del producto
+     * @param category - Categoría del producto
+     * @param status - Estado del producto (true = en stock, false = agotado)
+     */
     public Product(int id, String name, double quantity, int price, String description, String category, boolean status) 
     {
         this.id = id;
@@ -21,7 +68,8 @@ public class Product
         this.category = category;
         this.status = status;
     }
-//getters y setters
+    
+    // Getters y Setters
     public int getId()
     {
         return id;
@@ -92,6 +140,11 @@ public class Product
         this.status = true;
     }
 
+    // Methods
+    /**
+     * Método toString para representar el producto como una cadena de texto con sus atributos separados por comas
+     * @return String - Representación del producto en formato CSV (id,name,quantity,price,description,category,status)
+     */
     @Override
     public String toString()
     {
