b46e4f0f1db755ef95bcb8b5e5319c20fe6b6563
EngelAZ <engel.zeledon26@gmail.com>
2026-02-12 13:29:38 -0600

    Two folders were created in the view folder, one called dashboard and the other products. Inside the product folder, two classes were created, one called AddProductView and the other SearchProductView, and inside the dashboard folder, a class called DashboardView was created.

---
+++ src/view/dashboard/DashboardView.java
@@ -0,0 +1,9 @@
+package view.dashboard;
+
+import javax.swing.JPanel;
+
+public class DashboardView extends JPanel {
+    public DashboardView() {
+        // Aquí se podra configurar el panel y agregar los componentes necesarios para la vista del dashboard
+    }
+}
---
+++ src/view/products/AddProductView.java
@@ -0,0 +1,9 @@
+package view.products;
+
+import javax.swing.JPanel;
+
+public class AddProductView extends JPanel {
+    public AddProductView() {
+        // Aquí se podra configurar el panel y agregar los componentes necesarios para la vista de agregar producto
+    }
+}
