7c0fe557a612f3d8249f7a6ae6700e0777815cfa
EngelAZ <engel.zeledon26@gmail.com>
2026-02-16 15:57:48 -0600

    a

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-import controller.Controller;
 import java.awt.Color;
 import view.Window;
 
 public class Main {
     public static void main(String[] args) {
-
-        Window window = new Window(700, 1000, "Invent System", Color.WHITE, true);
-        window.isVisible();
-        Controller c = new Controller();
-        c.menu();
+        Window window = new Window(1000, 800, "Inventory System",Color.WHITE,true);
+        window.setVisible(true);
+       // Controller c = new Controller();
+      //  c.menu();
     }
 }
